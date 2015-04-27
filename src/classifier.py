ce03604cc53a5e7b1eed56d74bf6e1013ae87085
Bry Power <bapower57@gmail.com>
2015-04-27 13:04:58 -0400

    train and probability uses database

--- src/classifier.py
+++ src/classifier.py
@@ -32,19 +32,12 @@ class Classifier:
 		else:
 			return 0.59416
 
-	def probabilities_dict (self, action, round, board_rank = 7):
+	def probabilities_dict (self, action, round, board_rank = 8):
 		ratios = []
 		probabilities={}
 		for rank in range(1, 9):
 			prior_probability = self.prior_probability_rank(rank)
-			if (round == "pre_flop"):
-				prob = self.player.get_probability(action, rank, round)
-			elif (round == "flop"):
-				prob = self.player.flop_actions[action][flop_rank][rank]
-			elif (round == "turn"):
-				prob = self.player.flop_actions[action][flop_rank][rank]
-			else:
-				prob = self.player.flop_actions[action][flop_rank][rank]
+			prob = self.player.get_probability(action, rank, round, board_rank)
 			prob_ratio = prior_probability*prob
 			ratios.append(prob_ratio)
 		normalizer = functools.reduce(lambda x, y: x + y, ratios)
@@ -72,34 +65,22 @@ class Classifier:
 
 	# give the action and the rank the player had train the probabilities
 	# and return the new probability of that rank given that action
-	def train (self, action, rank, round, board_rank = 7):
-		params = (self.player.name, action, rank, 1)
-		if (round == "pre_flop"):
-			self.cursor.execute('SELECT probability FROM probabilities LEFT JOIN players ON probabilities.player = players.id WHERE players.name = ? AND action = ? AND hand_rank = ? AND round = ?', params)
-			data = self.cursor.fetchall()
-			if len(data)==0:
-				cols = (self.player.get_id()[0], action, round, board_rank, rank)
-				self.db.execute('INSERT INTO probabilities (player, action, round, board_rank, hand_rank, probability) VALUES (?, ?, ?, ?, ?, 1)', cols)
-			else:
-				self.db.execute('UPDATE probabilities SET probability = probability + 1 LEFT JOIN players ON probabilities.player = players.id  WHERE players.name = ? AND action = ? AND hand_rank = ? AND round = ?', params)
-		elif (round == "flop"):
-			self.player.flop_actions[action][board_rank][rank] += 1
-		elif (round == "turn"):
-			self.player.flop_actions[action][flop_rank][rank] += 1
-		else:
-			self.player.flop_actions[action][flop_rank][rank] += 1
+	def train (self, action, rank, round, board_rank = 8):
+		try:
+			cols = (self.player.get_id()[0], action, round, board_rank, rank)
+			self.db.execute('INSERT INTO probabilities (player, action, round, board_rank, hand_rank, probability) VALUES (?, ?, ?, ?, ?, 1)', cols)
+		except IntegrityError:
+			cols = (self.player.name, action, rank, round)
+			self.db.execute('UPDATE probabilities SET probability = probability + 1 LEFT JOIN players ON probabilities.player = players.id  WHERE players.name = ? AND action = ? AND hand_rank = ? AND round = ?', cols)
 		return self.player.get_probability(action, rank, round, board_rank)
 
-
     # predicting the test set
-
-
-    def predict_probability(self, test):
-        probabilities = []
-        for i in range(len(test)):
-            result = predict(self, test[i])
-            probabilities.append(result)
-        return probabilities
+	def predict_probability(self, test):
+		probabilities = []
+		for i in range(len(test)):
+			result = predict(self, test[i])
+			probabilities.append(result)
+		return probabilities
 
 
     # def mean(action):
@@ -111,9 +92,9 @@ class Classifier:
     #    return math.sqrt(variance)
 
 
-    def accuracy(test, probabilities):
-        exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
-        return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
+	def accuracy(test, probabilities):
+		exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
+		return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
 
 #   def normalize(possibilities):
 #        possibility_sum = sum(possibilities)
--- src/player.py
+++ src/player.py
@@ -7,67 +7,61 @@ class Player:
 
 	def __init__(self, name):
 		self.name = name
-		db = sqlite3.connect('pokerbot.db')
-		cur = db.cursor()
-		id = db.execute('SELECT id FROM players WHERE name = ?', (name,))
-		cur.execute('SELECT action, hand_rank, probability FROM probabilities LEFT JOIN players ON probabilities.player = players.id WHERE name = ? AND round = ?', (name, 1))
-		pre_flop_actions = []
-		columns = tuple( [d[0] for d in cur.description] )
-		for row in cur:
-		  pre_flop_actions.append(dict(zip(columns, row)))
-		cur.close()
-		db.close()
 
 	def get_id(self):
-		self.cur.execute('SELECT id FROM players WHERE name = ?', (self.name,))
+		self.cur.execute('SELECT id FROM players WHERE name = ?', (name,))
 		return self.cur.fetchone()
-		
-	def get_pre_flop(self):
-		cur.execute('SELECT action, hand_rank, probability FROM probabilities LEFT JOIN players ON probabilities.player = players.id WHERE name = ? AND round = ?', (name, 1))
-		pre_flop_actions = []
-		columns = tuple( [d[0] for d in cur.description] )
-		for row in cur:
-		  pre_flop_actions.append(dict(zip(columns, row)))
-		return pre_flop_actions
+
+	def get_trained_data(self, round, board_rank=8):
+		self.cur.execute('SELECT action, hand_rank, probability, board_rank FROM probabilities LEFT JOIN players ON probabilities.player = players.id WHERE name = ? AND round = ? AND board_rank = ?', (self.name, round, board_rank))
+		actions = []
+		columns = tuple( [d[0] for d in self.cur.description] )
+		for row in self.cur:
+		  actions.append(dict(zip(columns, row)))
+		return actions
+
+	def get_round_id (round):
+		if (round == "pre_flop"):
+			return 1
+		elif (round == "flop"):
+			return 2
+		elif (round == "turn"):
+			return 3
+		elif (round == "river"):
+			return 4
+		else:
+			raise Exception('not a round')
+
+	def get_action_id (action):
+		if (action == "raised"):
+			return 1
+		elif (action == "folded"):
+			return 2
+		elif (action == "called"):
+			return 3
+		elif (action == "bet"):
+			return 4
+		elif (action == "checked"):
+			return 5
+		else:
+			raise Exception('not an action')
+
+	#def board_rank ():
 
 	# get the number of times this player has been trained for a given action
-	def times_trained (self, action, round, flop_rank = 7):
-		if (round == "pre_flop"):
-			pre_flop_actions = self.get_pre_flop()
-			total = 0
-			for row in pre_flop_actions:
-				if (row['action'] == action):
-					total += row['probability']
-			return total
-		elif (round == "flop"):
-			total = 0
-			for row in self.pre_flop_actions:
-				if (row['action'] == action):
-					total += row['probability']
-			return total
-		elif (round == "turn"):
-			total = 0
-			for row in self.pre_flop_actions:
-				if (row['action'] == action):
-					total += row['probability']
-			return total
-		else:
-			total = 0
-			for row in self.pre_flop_actions:
-				if (row['action'] == action):
-					total += row['probability']
-			return total
+	def times_trained (self, action, round, board_rank=8):
+		actions = self.get_trained_data(round, board_rank)
+		total = 0
+		for row in actions:
+			if (row['action'] == action):
+				total += row['probability']
+		return total
+
 	# get the probability of a rank given the action
-	def get_probability(self, action, rank, round, board_rank=7):
-		if (round == "pre_flop"):
-			prob = 0
-			for row in self.pre_flop_actions:
-				if (row['action'] == action):
-					if (row['hand_rank'] == rank):
-						return row['probability'] / self.times_trained(action, round)
-		elif (round == "flop"):
-			return self.actions[action].get(rank) / self.times_trained(action, round, board_rank)
-		elif (round == "turn"):
-			return self.actions[action].get(rank) / self.times_trained(action, round, board_rank)
-		else:
-			return self.actions[action].get(rank) / self.times_trained(action, round, board_rank)
+	def get_probability(self, action, rank, round, board_rank=8):
+		actions = self.get_trained_data(round, board_rank)
+		prob = 0
+		for row in actions:
+			if (row['action'] == action):
+				if (row['hand_rank'] == rank):
+					return row['probability'] / self.times_trained(action, round, board_rank)
